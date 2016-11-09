a05c430dc00947a0da8cc247dcf15b2bf8e10a51
Sw4mp_fox <Sw4mpf0x@users.noreply.github.com>
2016-11-09 14:39:53 -0500

    Update urls.py

--- Kraken/Web_Scout/urls.py
+++ Kraken/Web_Scout/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'Web_Scout'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^inventory/$', views.inventory, name='inventory'),
     url(r'^setup/$', views.setup, name='setup'),
     url(r'^viewer/$', views.viewer, name='viewer'),
     url(r'^task_state$', views.task_state, name='task_state'),
