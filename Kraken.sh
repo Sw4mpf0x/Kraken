c3b5585f388bc4b1e32477b4b2d7157ce96b09c7
Sw4mp_fox <andrew.eric.luke@gmail.com>
2016-12-27 11:05:51 -0800

    Update

--- Kraken.sh
+++ Kraken.sh
@@ -1,14 +1,13 @@
 #/bin/bash
 
 display_usage(){
-    clear
-    echo "============================================================"
-    echo "This Kraken utility is used to start and stop services"
-    echo "required to run Kraken, as well as update Kraken with the"
-    echo "latest version."
+    echo "====================================================================="
+    echo "This Kraken utility is used to start and stop Kraken,"
+    echo "update Kraken, backup, restore, and add new users."
     echo ""
-    echo "Usage: $0 [start|stop|restart|update|adduser <username> <password>]"
-    echo "============================================================"
+    echo "Usage: $0 [start|stop|restart|update|backup|"
+    echo "           restore|adduser <username> <password>]"
+    echo "====================================================================="
 }
 
 if [ $# -le 0 ]
@@ -36,41 +35,53 @@ stop(){
 }
 
 update(){
-    printf "\033[1;31mWARNING! This will delete your Kraken database!\033[0m\n"
+    printf "\033[1;31mWARNING! Are you sure you want to update?\033[0m\n"
     read -p "Press [enter] to continue or Ctrl+c to cancel."
-    printf "\033[1;31mStopping Kraken\033[0m\n"
+    printf "\033[1;31mStopping Kraken...\033[0m\n"
     stop
+    backup
+
     rm -rf /tmp/Kraken
-    printf "\033[1;31mDownloading latest version of Kraken\033[0m\n"
+    printf "\033[1;31mDownloading latest version of Kraken...\033[0m\n"
     git clone https://github.com/Sw4mpf0x/Kraken.git /tmp/Kraken
+
     rm -rf /opt/Kraken
     mv /tmp/Kraken/Kraken /opt/
     secretkey=$(echo 'import random;print "".join([random.SystemRandom().choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for i in range(50)])' | python)
     echo SECRET_KEY = \'$secretkey\' >> /opt/Kraken/Kraken/settings.py
-    /opt/Kraken/manage.py makemigrations
-    /opt/Kraken/manage.py migrate
+    #/opt/Kraken/manage.py makemigrations
+    #/opt/Kraken/manage.py migrate
+    
+    printf "\033[1;31mRestoring Kraken data...\033[0m\n"
+    cwd=$(pwd)
+    echo $cwd/KrakenBackup.zip | restore
+
+
     chown -R www-data /opt/Kraken
     chgrp -R www-data /opt/Kraken
     chmod 775 /opt/Kraken/Kraken/
     chmod 775 /opt/Kraken/Kraken/kraken.db
-    chmod 775 /opt/Kraken/Web_Scout/static/Web_Scout/
+    chmod 775 /opt/Kraken/static/Web_Scout/
     chmod 775 /opt/Kraken/ghostdriver.log
     chmod 775 /opt/Kraken/tmp/
-    echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@kraken.com', '2wsxXSW@')" | python /opt/Kraken/manage.py shell
+    #echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@kraken.com', '2wsxXSW@')" | python /opt/Kraken/manage.py shell
     printf "\033[1;31mStarting Kraken\033[0m\n"
-    chmod 755 /tmp/Kraken/update.sh
-    if [ -a "/tmp/Kraken/update.sh" ]
+    if [ -e "/tmp/Kraken/update.sh" ]
         then
             chmod 755 /tmp/Kraken/update.sh
             /tmp/Kraken/update.sh
     fi
     start
+
+
     rm -rf /tmp/Kraken
+    rm $cwd/KrakenBackup.zip
     printf "\033[1;31mUpdate complete!\033[0m\n"
 }
 
 backup(){
-    zip -j KrakenBackup.zip /opt/Kraken/Kraken/kraken.db /opt/Kraken/Web_Scout/static/Web_Scout/*
+    printf "\033[1;31mBacking up Kraken to current directory...\033[0m\n"
+    zip -j KrakenBackup.zip /opt/Kraken/Kraken/kraken.db /opt/Kraken/static/Web_Scout/*
     printf "\033[1;31mBackup saved to KrakenBackup.zip in current working directory.\033[0m\n"
 }
 
@@ -79,16 +90,22 @@ restore(){
     do
         echo "Specify absolute path to KrakenBackup.zip: "
         read backuppath
+        if [ ! -e "$backuppath" ]; then
+            echo "Invalid path entered."
+            backuppath=""
+        fi
     done
 
     if [ ! -d "/tmp/krakenbackup" ]
         then
             mkdir /tmp/krakenbackup
     fi
+    printf "\033[1;31mRestoring Kraken data...\033[0m\n"
     unzip $backuppath -d /tmp/krakenbackup/
     cp /tmp/krakenbackup/kraken.db /opt/Kraken/Kraken/kraken.db
-    cp /tmp/krakenbackup/*.png /opt/Kraken/Web_Scout/static/Web_Scout/
+    cp /tmp/krakenbackup/*.png /opt/Kraken/static/Web_Scout/
     rm -rf /tmp/krakenbackup
+    printf "\033[1;31mRestoration complete.\033[0m\n"
 }
 
 adduser(){
@@ -118,7 +135,7 @@ case "$1" in
         stop
         ;;
     reset|restart)
-        printf "\033[1;31mResetting Kraken.\033[0m\n"
+        printf "\033[1;31mRestarting Kraken.\033[0m\n"
         stop
         start
         ;;
